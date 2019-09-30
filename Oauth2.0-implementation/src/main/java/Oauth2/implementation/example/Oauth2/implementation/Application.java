28addc06c5758db7c16855f410961d15400c5339
Shambhavi Tewari <33371065+shambhavitewari@users.noreply.github.com>
2019-09-30 13:31:16 +0530

    Add files via upload

---
+++ Oauth2.0-implementation/src/main/java/Oauth2/implementation/example/Oauth2/implementation/Application.java
@@ -0,0 +1,13 @@
+package Oauth2.implementation.example.Oauth2.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
